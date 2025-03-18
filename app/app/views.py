c3885f40b77b82dbd623009fa4377d4160258fd7
Ardee Aram <ardee.aram@gmail.com>
2025-03-18 10:35:53 +0800

    It works!

--- app/app/views.py
+++ app/app/views.py
@@ -7,23 +7,42 @@ import json
 
 def home(request):
   if request.method == 'GET':
+
     return render(request, 'app/index.html', {})
+
+  # POSTback (old school)
+  # We can do AJAX style, but it will make this tutorial 
+  # more complicated than it should be.
   elif request.method == 'POST':
+
     query = request.POST.get('query')
     bedrock_client = Bedrock()
     response = bedrock_client.retrieve_and_generate(query)
     
     message = response['output']['text']
     citations = response["citations"]
-    
-    contexts = []
-    for citation in citations:
-        retrievedReferences = citation["retrievedReferences"]
-        for reference in retrievedReferences:
-            contexts.append(reference["content"]["text"])
+    retrieved_references = [citation["retrievedReferences"] for citation in citations]
 
 
-    pprint(response)
-    pprint(message)
+    #Flatten retrieved_references
+    flat_retrieved_references = [item for retrienved_reference_item in retrieved_references for item in retrienved_reference_item]
+  
+    references = set([
+      f'''
+        <div>
+          <blockquote>...{flat_retrieved_references_item['content']['text']}...</<blockquote>
+          <cite>&mdash; {flat_retrieved_references_item['location']['s3Location']['uri']}</cite>
+        </div>
+      '''
+      for flat_retrieved_references_item in flat_retrieved_references
+    ])
 
-    return render(request, 'app/index.html', {'query': query, 'message': json.dumps(message)})
+    references_string = " ".join(references)
+
+    return render(request, 'app/index.html', {
+      'query': query, 
+      'message': json.dumps(message),
+      'references': json.dumps(references_string),
+      'references_count': len(references)
+    }) 
+      
